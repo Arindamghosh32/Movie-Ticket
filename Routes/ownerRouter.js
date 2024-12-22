844589c5aaf1d0d02569d2519b8b49c8b265ea3d
Arindamghosh32 <ghosharindam2822@gmail.com>
2024-12-22 18:22:08 +0530

    I have done creation of movie seatings from owner

--- Routes/ownerRouter.js
+++ Routes/ownerRouter.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const moviesModel = require('./../models/moviesModel');
 
 router.get('/create',(req,res)=>{
     res.render('Create');
 });
+
+router.post('/created',async(req,res)=>{
+    try{
+        const created = await moviesModel.create(req.body);
+        res.status(201).json(created);
+    }catch(error){
+       res.status(400).json({error: error.message});
+    }  
+})
 
 module.exports = router;
