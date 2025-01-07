3a744cc5f6a02ef00503eeb84903672e42d48344
Arindamghosh32 <ghosharindam2822@gmail.com>
2025-01-07 15:00:35 +0530

    Fetching the movie details in my home page and help to navigate to its about

--- Routes/userRouter.js
+++ Routes/userRouter.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const moviesModel = require('./../models/moviesModel')
 const { isLoggedin } = require('./../middleware/isLoggedin');
 
-router.get('/home',isLoggedin,(req,res)=>{
-    res.render('Home');
+router.get('/home',isLoggedin,async(req,res)=>{
+    const movies = await moviesModel.find();
+    res.render('Home',{movies});
+    console.log('The movies data sent in home is',movies);
+});
+
+router.get('/movie/:id',async(req,res)=>{
+  const movieId = req.params.id;
+  try{
+     const movie = await moviesModel.findById(movieId);
+     if(movie){
+        res.render("Movie",{movie});
+     }else{
+        console.log("There is an error storing the data");
+     }
+  }catch(e){
+    console.error(e);
+  }
 })
 
 module.exports = router;
